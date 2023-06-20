972454e3a74efb987daf802462ba3164495738b1
saravananhashref <saravanan@hashref.in>
2023-06-20 12:32:03 +0530

    new

--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import axios from 'axios';
 
 export const Home = () => {
+
+    const create=(e)=>{
+        console.log('create');
+        e.preventDefault();
+        try {
+            axios.post('http://localhost:3333/login')
+            .then()
+            .catch();
+        } catch (error) {
+            
+        }
+    }
+
     return (
         <div className='home'>
             <h1>Home</h1>
+            <button type="button" onClick={create}>create</button>
         </div>
     );
 };
